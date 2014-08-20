7ef4c05beddcf5723ada051c52ff17bfb286019d
Edwin Zhang <ehzhang@mit.edu>
2014-08-20 17:24:10 -0500

    site is basically complete, just needs images :)

--- js/site.js
+++ js/site.js
@@ -1,3 +1,36 @@
+function takeover(){
+  var $content = $('.screen .content');
+
+  $content.addClass('nyan');
+
+  setTimeout(function(){
+    $content.removeClass('nyan');
+  }, 10000)
+}
+
+function alert(){
+  var $alerts = $('.alert');
+
+  $alerts.show();
+
+  setTimeout(function(){
+    $alerts.hide();
+  }, 10000)
+}
+
+function change(num, value){
+  $('#gscreen-' + num)
+      .children('.content')
+      .removeClass()
+      .addClass('content ' + value)
+}
+
 $(document).ready(function(){
   new WOW().init();
+
+  $('select.channel').change(function(e){
+    var value = e.target.value;
+    change(4, value);
+  })
+
 });
